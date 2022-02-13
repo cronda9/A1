583ccdec808fdd856ad567942d16870eb1a6316b
Christian Ronda <cronda@princeton.edu>
2022-02-13 17:51:02 -0500

    final commit

--- reg.py
+++ reg.py
@@ -1,3 +1,7 @@
+"""
+reg.py
+Authors: Christian Ronda and Gracyn Kuerner
+"""
 from sys import stderr, exit
 from contextlib import closing
 from sqlite3 import connect
@@ -7,60 +11,65 @@ import argparse
 DATABASE_URL = "file:reg.sqlite?mode=ro"
 
 def main():
+    """Princeton Registar Class Search"""
     tags = []
 
     parser = argparse.ArgumentParser(description='Register application: show overviews of classes', allow_abbrev=False)
 
-    parser.add_argument('-d', type = str, help = 'show only those classes whose department contains dept', action = 'store', metavar='dept')
-    parser.add_argument('-n', type = str, help = 'show only those classes whose course number contains num', action = 'store', metavar='num')
-    parser.add_argument('-a', type = str, help = 'show only those classes whose distrib area contains area', action = 'store', metavar='area')
-    parser.add_argument('-t', type = str, help = 'show only those classes whose course title contains title', action = 'store', metavar='title')
+    help_str = 'show only those classes whose department contains '
+    parser.add_argument('-d', type = str, help = help_str+'dept', action = 'store', metavar='dept')
+    parser.add_argument('-n', type = str, help = help_str+'num', action = 'store', metavar='num')
+    parser.add_argument('-a', type = str, help = help_str+'area', action = 'store', metavar='area')
+    parser.add_argument('-t',type = str, help = help_str+'title', action = 'store', metavar='title')
 
     dept = parser.parse_args().d
     num = parser.parse_args().n
     area = parser.parse_args().a
     title = parser.parse_args().t
 
-    title = title.replace('%', r'%')
-    title = title.replace('_', r'_')
-
     try:
         with connect(DATABASE_URL, isolation_level=None, uri=True) as connection:
             with closing(connection.cursor()) as cursor:
 
-                stmt =  "SELECT classes.classid, "
-                stmt +=         "crosslistings.dept, "
-                stmt +=         "crosslistings.coursenum, "
-                stmt +=         "courses.area, "
-                stmt +=         "courses.title "
+                stmt =  "SELECT classes.classid, crosslistings.dept, crosslistings.coursenum, "
+                stmt +=         "courses.area, courses.title "
                 stmt += "FROM crosslistings, courses, classes "
                 stmt += "WHERE courses.courseid = crosslistings.courseid "
                 stmt += "AND classes.courseid = crosslistings.courseid "
 
                 if dept:
+                    dept = dept.replace('%', r'\%')
+                    dept = dept.replace('_', r'\_')
                     tags.append("%" + dept.lower() + "%")
-                    stmt += "AND lower(crosslistings.dept) like ? "
+                    stmt += "AND lower(crosslistings.dept) like ? ESCAPE '\\' "
 
                 if num:
+                    num = num.replace('%', r'\%')
+                    num = num.replace('_', r'\_')
                     tags.append("%" + num.lower() + "%")
-                    stmt += "AND lower(crosslistings.coursenum) like ? "
+                    stmt += "AND lower(crosslistings.coursenum) like ? ESCAPE '\\' "
 
                 if area:
+                    area = area.replace('%', r'\%')
+                    area = area.replace('_', r'\_')
                     tags.append("%" + area.lower() + "%")
-                    stmt += "AND lower(courses.area) like ? "
+                    stmt += "AND lower(courses.area) like ? ESCAPE '\\' "
 
                 if title:
+                    title = title.replace('%', r'\%')
+                    title = title.replace('_', r'\_')
                     tags.append("%" + title.lower() + "%")
-                    stmt += "AND lower(courses.title) like ? "
+                    stmt += "AND lower(courses.title) like ? ESCAPE '\\'"
 
                 stmt += "ORDER BY crosslistings.dept, crosslistings.coursenum, classes.classid;"
 
                 cursor.execute(stmt, tags)
 
                 row = ["-----", "----", "------", "----", "-----"]
-                print("{0:5} {1:4} {2:6} {3:4} {4}".format("ClsId", "Dept", "CrsNum", "Area", "Title"))
+
+                print("ClsId Dept CrsNum Area Title")
                 while row:
-                    text = "{0:>5} {1:>4} {2:>6} {3:>4} {4}".format(row[0], row[1], row[2], row[3], row[4])
+                    text = f"{row[0]:>5} {row[1]:>4} {row[2]:>6} {row[3]:>4} {row[4]}"
                     print(textwrap.fill(text, initial_indent='', subsequent_indent=' ' * 23, break_long_words=False, width=72,))
                     row = cursor.fetchone()
 
---
+++ regdetails.py
@@ -0,0 +1,81 @@
+"""
+reg_details.py
+Authors: Christian Ronda and Gracyn Kuerner
+"""
+from sys import  stderr, exit
+from contextlib import closing
+from sqlite3 import connect
+import textwrap
+import argparse
+
+DATABASE_URL = "file:reg.sqlite?mode=rwc"
+
+def main():
+    """Princeton Registar Class Details Search"""
+
+    parser = argparse.ArgumentParser(description='Registrar application: show details about a class', allow_abbrev=False)
+
+    parser.add_argument('classid', metavar='classid',type = int, help = 'the id of the class whose details should be shown')
+
+    class_id = parser.parse_args().classid
+
+    try:
+        with connect(DATABASE_URL, isolation_level=None, uri=True) as connection:
+            with closing(connection.cursor()) as cursor:
+                stmt = "SELECT courseid, days, starttime, endtime, bldg, roomnum FROM classes where classes.classid = ? "
+                class_info = cursor.execute(stmt, [class_id]).fetchall()
+
+                if not class_info:
+                    raise Exception("no class with classid "+str(class_id)+" exists")
+
+                class_info = class_info[0]
+
+                courseid = class_info[0]
+
+                stmt="SELECT area, title, descrip, prereqs FROM courses WHERE courses.courseid = ? "
+                course_info = cursor.execute(stmt, [courseid]).fetchall()[0]
+
+                stmt="SELECT dept, coursenum FROM crosslistings WHERE crosslistings.courseid = ? "
+                stmt += "ORDER BY dept, coursenum"
+                cross_info = cursor.execute(stmt, [courseid]).fetchall()
+
+                stmt =  "SELECT profs.profname FROM profs, coursesprofs "
+                stmt += "WHERE coursesprofs.courseid = ? "
+                stmt += "AND profs.profid = coursesprofs.profid "
+                stmt += "ORDER BY profs.profname "
+                profs_info = cursor.execute(stmt, [courseid]).fetchall()
+
+                print('Course Id: ' + str(class_info[0]) + '\n')
+                print('Days:', class_info[1])
+                print('Start time:', class_info[2])
+                print('End time:', class_info[3])
+                print('Building:', class_info[4])
+                print('Room: ' + str(class_info[5]) + '\n')
+
+                for row in cross_info:
+                    print('Dept and Number:', row[0], row[1])
+
+                print('\nArea: ' + str(course_info[0]) + '\n')
+
+                text = 'Title: ' + course_info[1]
+                print(textwrap.fill(text, initial_indent='', break_long_words=False, width=72))
+                print()
+
+                text = 'Description: ' + course_info[2]
+                print(textwrap.fill(text, initial_indent='', break_long_words=False, width=72))
+                print()
+
+                text = 'Prerequisites: ' + course_info[3]
+                print(textwrap.fill(text, initial_indent='', break_long_words=False, width=72))
+                print()
+
+                if profs_info:
+                    for row in profs_info:
+                        print('Professor:', row[0])
+
+    except Exception as ex:
+        print('reg_details.py: '+ex, file=stderr)
+        exit(1)
+
+if __name__ == "__main__":
+    main()
